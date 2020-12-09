8c121bcd4c65e135b099a787bb374d8e6ed2ddac
stuartErnstsen <stuarternstsen@gmail.com>
2020-12-08 23:11:49 -0700

    Added black diamond challenge logic to tic-tac-toe and poker

--- Part1/tic-tac-toe.js
+++ Part1/tic-tac-toe.js
@@ -1,18 +1,52 @@
-const board = [];
+let board = [];
+let boardSize = 9;
 let turnCount = 0;
 const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-let boardSize = document.getElementById('')
 let winner = false;
+let endAfterWin = false;
 let play = (boxNum) => {
+    //Set element that is at box index that was clicked to a variable 
     const boxClicked = document.getElementById(`${boxNum}`);
+    //Get text if there is any in the box to be used in check for player occupied
+    const boxText = boxClicked.textContent;
     let currPlayer = document.getElementById('player');
     let currPLayerText = currPlayer.textContent;
-    board[boxNum] = currPLayerText;
-    turnCount++;
-    console.log(board)
-    console.log(turnCount)
-    //Add "X" or "O" text to box that was clicked
-    boxClicked.textContent = currPLayerText;
+    //Check if there is already a player occupying box, if not, add current player to the board.
+    if (!boxText && !endAfterWin) {
+        //Add "X" or "O" text to box that was clicked
+        boxClicked.textContent = currPLayerText;
+        //Add current player text to board array at clicked box index to check win conditions
+        board[boxNum] = currPLayerText;
+        turnCount++;
+        if (!winner) {
+            //Win check will iterate through winCombo. If a win combo has been completed the win boolean will always return true; If not it will return the win check of the next array in winCombo.
+            winner = winCombos.reduce((win, arr) => {
+                if (win) {
+                    return win;
+                }
+                return win = arr.reduce((comboWin, num, index) => {
+                    // If comboWIn is false and the current index is not the first element the win check has failed an will return false for the rest of the array
+                    if (!comboWin && index > 0) {
+                        return false;
+                    }
+                    return comboWin = board[num] === currPLayerText ? true : false;
+                }, false);
+            }, false);
+        }
+        if (!winner && turnCount === 9) {
+            window.alert("Cat's Game");
+            resetBoard();
+        }
+        if (winner) {
+            //Get value for checkbox to lock game after someone wins.
+            endAfterWin = document.getElementById('end').checked;
+            window.alert(`${currPLayerText} is the winner!`);
+            resetBoard();
+        }
+        // Once a box is clicked the current player's name will be updated to the other player
+        currPlayer.textContent = currPLayerText === "X" ? "O" : "X";
+    }
+
     //Check if the click was a winner!
     // if ((board[0] === currPLayerText && board[1] === currPLayerText && board[2] === currPLayerText) ||
     //     (board[3] === currPLayerText && board[4] === currPLayerText && board[5] === currPLayerText) ||
@@ -23,31 +57,15 @@ let play = (boxNum) => {
     //     (board[0] === currPLayerText && board[4] === currPLayerText && board[8] === currPLayerText) ||
     //     (board[2] === currPLayerText && board[4] === currPLayerText && board[6] === currPLayerText)) {
     //     winner = true;
-
     // }
-    if (!winner) {
-        //Win check will iterate through winCombo. If a win combo has been completed the win boolean will always return true; If not it will check the not it will return the win check of the next array in winCombo.
-        winner = winCombos.reduce((win, arr) => {
-            if (win) {
-                return win;
-            }
-            return win = arr.reduce((comboWin, num, index) => {
-                // If comboWIn is false and the current index is not the first element the win check has failed an will return false for the rest of the array
-                if (!comboWin && index > 0) {
-                    return false;
-                }
-                return comboWin = board[num] === currPLayerText ? true : false;
-            }, false);
-        }, false);
-    }
-    if (!winner && turnCount === 9) {
-        window.alert("Cat's Game");
-    }
-    if (winner) {
-        window.alert(`${currPLayerText} is the winner!`);
-    }
-    // Once a box is clicked the current player's name will be updated to the other player
-    currPlayer.textContent = currPLayerText === "X" ? "O" : "X";
 
+}
 
+const resetBoard = () => {
+    winner = false;
+    board = [];
+    turnCount = 0;
+    for (let i = 0; i < boardSize; i++) {
+        document.getElementById(`${i}`).innerText = '';
+    }
 }
--- Part2/poker.js
+++ Part2/poker.js
@@ -10,11 +10,10 @@ const defaultHand = [{ suitType: "diamonds", suitIcon: '♦', cardType: 'A', col
 { suitType: "hearts", suitIcon: '♥', cardType: 'A', color: 'grey', default: true },
 { suitType: "spades", suitIcon: '♠', cardType: 'A', color: 'grey', default: true },
 { suitType: "clubs", suitIcon: '♣', cardType: 'A', color: 'grey', default: true }];
-const deckCardTypes = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
-const deckCardLength = 13
-const deckCardSuits = ['hearts', 'diamonds', 'spades', 'clubs']
+const deckCardTypes = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+const deckCardLength = 13;
+const deckCardSuits = ['hearts', 'diamonds', 'spades', 'clubs'];
 const deckSuitLength = 4;
-const deckHandLength = 4;
 let cardCount = 0;
 
 const setCard = () => {
@@ -25,20 +24,20 @@ const setCard = () => {
 const setHand = () => {
     //Copy structure of default hand
     //Randomize properties of Card objects of newHand
-    let newHand = [];
-    defaultHand.map(item => newHand.push(Object.assign({}, item)));
+    let newHand = defaultHand.map(i => ({ ...i }))
+    // defaultHand.map(item => newHand.push(Object.assign({}, item)));
     newHand = newHand.map((card, index) => {
         //Check if there are unused cards left in the deck to make new hand, if not deck will reset
         if (cardCount === 52) {
             shuffleDeck();
-        }
+        };
         cardCount++;
         //Check if card is a duplicates before adding card to hand, will create a new card if card already is in the deck;
         while (!checkCard(card) || card.default) {
             card.suitType = randomSuit();
             card.cardType = randomCard();
             card.default = false;
-        }
+        };
         //Change value for default card to false to indicate new card has been dealt;
 
         //Loop through default deck suit properties to get color/icon that matches with suit type. 
@@ -46,77 +45,49 @@ const setHand = () => {
             if (suit.suitType === card.suitType) {
                 card.color = suit.color;
                 card.suitIcon = suit.suitIcon;
-            }
+            };
         });
-        //Set properties to current card (0-4) on html elements
-        //Attaching tempCard to corresponding card section on html
-        const tempCard = document.getElementsByClassName(`card${index}`)
-        //Setting color of entire element
-        tempCard[0].style.color = card.color;
-        //Attaching tempCardIcon to span tag inside section element to change icon on cards
-        const tempCardIcon = document.getElementById(`cardSuit${index}`)
-        tempCardIcon.innerText = card.suitIcon;
-        //Attaching tempCardType to text spans of card type to change card type on cards displayed
-        const tempCardType = document.getElementsByClassName(`cardType${index}`)
-        for (let i = 0; tempCardType[i]; i++) {
-            tempCardType.item(i).innerText = card.cardType;
-        }
-
+        updateBoard(card, index);
 
     });
-    // updateBoard(newHand);
 }
 
 const resetGame = () => {
     defaultHand.map((el, index) => {
-        //Set properties to current card (0-4) on html elements
-        //Attaching tempCard to corresponding card section on html
-        const tempCard = document.getElementsByClassName(`card${index}`)
-        //Setting color of entire element
-        tempCard[0].style.color = el.color;
-        //Attaching tempCardIcon to span tag inside section element to change icon on cards
-        const tempCardIcon = document.getElementById(`cardSuit${index}`)
-        tempCardIcon.innerText = el.suitIcon;
-        //Attaching tempCardType to text spans of card type to change card type on cards displayed
-        const tempCardType = document.getElementsByClassName(`cardType${index}`)
-        for (let i = 0; tempCardType[i]; i++) {
-            tempCardType.item(i).innerText = el.cardType;
-        }
+        updateBoard(el, index);
     });
     // updateBoard(defaultHand);
     shuffleDeck();
-}
+};
 
-// const updateBoard = (cardArray) => {
-//     console.log(cardArray)
-//     cardArray.map((el, index) => {
-//         //Set properties to current card (0-4) on html elements
-//         //Attaching tempCard to corresponding card section on html
-//         const tempCard = document.getElementsByClassName(`card${index}`)
-//         //Setting color of entire element
-//         tempCard[0].style.color = el.color;
-//         //Attaching tempCardIcon to span tag inside section element to change icon on cards
-//         const tempCardIcon = document.getElementById(`cardSuit${index}`)
-//         tempCardIcon.innerText = el.suitIcon;
-//         //Attaching tempCardType to text spans of card type to change card type on cards displayed
-//         const tempCardType = document.getElementsByClassName(`cardType${index}`)
-//         for (let i = 0; tempCardType[i]; i++) {
-//             tempCardType.item(i).innerText = el.cardType;
-//         }
-//     });
-// }
+const updateBoard = (el, index) => {
+    //Set properties to current card (0-4) on html elements
+    //Attaching tempCard to corresponding card section on html
+    const tempCard = document.getElementsByClassName(`card${index}`);
+    //Setting color of entire element
+    tempCard[0].style.color = el.color;
+    //Attaching tempCardIcon to span tag inside section element to change icon on cards
+    const tempCardIcon = document.getElementById(`cardSuit${index}`);
+    tempCardIcon.innerText = el.suitIcon;
+    //Attaching tempCardType to text spans of card type to change card type on cards displayed
+    const tempCardType = document.getElementsByClassName(`cardType${index}`);
+    for (let i = 0; tempCardType[i]; i++) {
+        tempCardType.item(i).innerText = el.cardType;
+    }
+
+}
 const randomNum = (range) => {
     return Math.floor(Math.random() * range);
-}
+};
 
 const randomCard = () => {
-    return card = deckCardTypes[randomNum(deckCardLength)]
-}
+    return card = deckCardTypes[randomNum(deckCardLength)];
+};
 
 const randomSuit = () => {
-    return suit = deckCardSuits[randomNum(deckSuitLength)]
+    return suit = deckCardSuits[randomNum(deckSuitLength)];
 
-}
+};
 
 const checkCard = (card) => {
     //Check if card holds values in cardType and suitType properties and if the card is not a default card value
@@ -125,7 +96,7 @@ const checkCard = (card) => {
             //Check the deck array for the matching suit and if the card type has already been used return false and if mark card used in the array with true and return true to checkCard
             if (deck[i].suitType === card.suitType) {
                 if (deck[i].suitDeck[deckCardTypes.indexOf(card.cardType)]) {
-                    return false
+                    return false;
                 }
                 deck[i].suitDeck[deckCardTypes.indexOf(card.cardType)] = true;
                 return true;
